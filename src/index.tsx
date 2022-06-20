63140e46e14d0c4ae134f95b7c171f72031854ed
chezhe <chezhe@hey.com>
2022-06-20 17:06:02 +0800

    fix export

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import Input from './components/Input'
+import Box from './components/Box'
+import Layout from './components/Layout'
+import RadioGroup from './components/RadioGroup'
+import CheckBoxGroup from './components/CheckBoxGroup'
+import Heading from './components/Heading'
+import SettingBlock from './components/SettingBlock'
+import Button from './components/Button'
+import Toast from './components/Toast'
+import ToastInstance from './components/Toast/Instance'
+import { Text, View } from './components/Themed'
+
+export {
+  Input,
+  Box,
+  Text,
+  View,
+  Button,
+  Toast,
+  RadioGroup,
+  CheckBoxGroup,
+  Heading,
+  SettingBlock,
+  Layout,
+  ToastInstance,
+}
